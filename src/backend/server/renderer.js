4830b826986f79aa6d742dd126d93c1c9e1a9eec
Maksim Skvortsov <maksskvor@yandex.ru>
2018-01-15 23:06:40 +0300

    add module alias

--- src/backend/server/renderer.js
+++ src/backend/server/renderer.js
@@ -1,10 +1,16 @@
 const React = require('react');
 const ReactDOM = require('react-dom/server');
-import Component from './Test.jsx';
+import configureStore from 'store';
+import { Provider } from 'react-redux';
+import App from 'frontend/App.jsx';
 
 module.exports = (req, res) => {
+    const store = configureStore();
+
     const componentHTML = ReactDOM.renderToString(
-        <Component />  
+        <Provider store={store}>
+            <App />
+        </Provider>  
     );   
     console.log(componentHTML);
     res.end(componentHTML);
